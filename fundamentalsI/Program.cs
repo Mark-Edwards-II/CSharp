027e158800e158478e060aef48430fc51df6a7bb
Robert Mark Edwards II <markedwards@MacBook-Pro.cable.rcn.com>
2020-11-30 14:40:17 -0600

    Third commit

---
+++ fundamentalsI/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace fundamentalsI
+{
+    class Program
+    {
+        public static void oneToTwoFiftyFive()
+        {
+            for(int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+        public static void threeOrFive()
+        {
+            for(int i = 1;i <= 100;i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    if(i % 3 != 0 || i % 5 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+        public static void fizzBuzz()
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                if( i % 3 == 0 || i % 5 == 0 )
+                {
+                    if(i % 3 == 0 && i % 5 != 0)
+                    {
+                        Console.WriteLine("Fizz " + i);
+                    }
+                    else if(i % 5 == 0 && i % 3 !=0)
+                    {
+                        Console.WriteLine("Buzz " + i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("FizzBuzz " + i);
+                    }
+                }
+            }
+        }
+        public static void whileOneToTwoFiftyFive()
+        {
+            int i = 1;
+            while(i <= 255)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+        public static void whileThreeOrFive()
+        {
+            int i= 1;
+            while(i <= 100)
+            {
+                if(i % 3 ==0 || i % 5 ==0 )
+                {
+                    if(i % 3 != 0 || i % 5 != 0){
+                        Console.WriteLine(i);
+                    }
+                }
+                i++;
+            }
+        }
+        public static void whileFizzBuzz()
+        {
+            int i =1;
+            while(i <= 100)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    if(i % 5 != 0)
+                    {
+                        Console.WriteLine("Fizz " + i);
+                    }
+                    else if(i % 3 != 0)
+                    {
+                        Console.WriteLine("Buzz " + i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("FizzBuzz " + i);
+                    }
+                }
+                i++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // oneToTwoFiftyFive();
+            // threeOrFive();
+            // fizzBuzz();
+            // whileOneToTwoFiftyFive();
+            // whileThreeOrFive();
+            // whileFizzBuzz();
+        }
+    }
+}
--- helloAgain/Program.cs
+++ helloAgain/Program.cs
@@ -6,7 +6,7 @@ namespace helloAgain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is {0}, I am " + 28 + " years old", "Tim");
         }
     }
 }
