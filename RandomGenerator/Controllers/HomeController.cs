5badd629646e39c09f8910aefd552d5e3f948f2f
Robert Mark Edwards II <markedwards@MacBook-Pro.local>
2020-12-12 13:43:33 -0600

    Random Passcode Generator

---
+++ RandomGenerator/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomGenerator.Models;
+
+namespace RandomGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetInt32("Count") == null)
+            {
+                HttpContext.Session.SetInt32("Count", 1);
+            }
+            int? count = HttpContext.Session.GetInt32("Count");
+            ViewBag.Count = count;
+
+            string Code = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            string NewString = "";
+            Random rand = new Random();
+            rand.Next(0, Code.Length);
+            for (var i = 0; i < 15; i++)
+            {
+                NewString += Code[rand.Next(0, Code.Length)];
+            }
+            ViewBag.NewString = NewString;
+            return View();
+        }
+
+        public IActionResult Increment()
+        {
+            int? count = HttpContext.Session.GetInt32("Count");
+            count++;
+            HttpContext.Session.SetInt32("Count", (int)count);
+            return RedirectToAction("Index");
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RandomGenerator/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// function RandNum()
+//         {
+//             const Code = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//             let NewString = "";
+//             for(var i = 0; i < 15; i++)
+//             {
+//                 let Math.floor(Math.random() * Code.length);  
+
+
+//             }
+//             return "passcode";
+//         }
+
+
+//         if(HttpContext.Session)
+//         int PasscodeCount = 0;
+//         string Supplies = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+//         string NewString = "";
+//         int Break = 14;
+//         int idx = 0;
+//         Random rand = new Random();
+//         while(idx <= Break)
+//         {
+//             NewString += Supplies[rand.Next(Supplies.Length)];
+//             idx++;
+//         }
+        
+//         return View(NewString);
